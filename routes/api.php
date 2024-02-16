5d82fc2e9f0f7f5fe62a35f4d1d591c99ac14bb3
Jorge Ramos <dev.jorgeramos@gmail.com>
2024-02-16 09:35:40 -0300

    limitando recursos

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controlers\AuthController;
 use App\Http\Controllers\Api\v1\CategoryController;
 use App\Http\Controllers\Api\v1\ProductController;
@@ -15,13 +13,18 @@ use App\Http\Controllers\Api\v1\ProductController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
-$this->group(['prefix' => 'v1', 'namespace' => 'Api\v1'], function (){
-    $this->post('auth', AuthController::class);
-    $this->post('auth-refresh', AuthController::refreshToken);
-    $this->get('me', AuthController::getAuthenticatedUser);
+$this->group([
+    'prefix' => 'v1',
+    'namespace' => 'Api\v1',
+    'middleware' => 'auth:api',
+],
+    function () {
+        $this->post('auth', AuthController::class);
+        $this->post('auth-refresh', AuthController::refreshToken);
+        $this->get('me', AuthController::getAuthenticatedUser);
 
-    $this->apiResource('categories', CategoryController::class);
-    $this->apiResource('products', ProductController::class);
-});
+        $this->apiResource('categories', CategoryController::class);
+        $this->apiResource('products', ProductController::class);
+    });
